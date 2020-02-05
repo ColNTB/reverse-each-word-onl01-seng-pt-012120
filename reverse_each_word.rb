0c62d7938b206525f6e6c1410941cdaad0b2f595
Nathan Bohn <nathantbohn@gmail.com>
2020-02-05 00:02:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,12 +4,7 @@ def reverse_each_word(string)
       array = string.split(" ")
       result = ''
       array.each do |word|
-         i = word.size 
-         while i  > 0 
-            result << word[i]
-            i -= 1 
-          end 
-          result << " "
+         result << word 
       end 
       return result 
 end 
