b3f4d5204a45b1510e362bb24ce40ff99580d90f
Nathan Bohn <nathantbohn@gmail.com>
2020-02-05 00:06:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,10 @@
 def reverse_each_word(string)
       array = string.split("")
       result = ''
+      array.each do |word|
+        result << word 
+      end 
+      return result 
      
 end 
 
